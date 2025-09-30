4dbd79f90c85f4af1158bfd6897766702471e9a1
ReactivePlayZ <reactiveplayz@gmail.com>
2025-09-30 20:47:47 +0600

    updated comments in TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -58,6 +58,7 @@ bool tieChecker(char grid[3][3]) {
 int emptyWinPos(char grid[3][3], char p) {
     // Returns the next move to win (rather the move that should be blocked by opponent of p)
     // Otherwise returns 0
+    // Doesn't check diagonals for now
     int count; int countY;
     int emptyPos; int emptyPosY;
     for (int i=0; i<3; i++) {
